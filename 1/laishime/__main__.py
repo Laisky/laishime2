52d37fa6a53816ff775351107e9b873bb68a2350
Laisky <ppcelery@gmail.com>
2014-12-06 11:59:59 +0800

    finished #2

--- 1/laishime/__main__.py
+++ 1/laishime/__main__.py
@@ -3,5 +3,5 @@ import wsgiref.simple_server
 from laishime import application
 
 
-server = wsgiref.simple_server.make_server('', 8080, application)
+server = wsgiref.simple_server.make_server('', 80, application)
 server.serve_forever()
