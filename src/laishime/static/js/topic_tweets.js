827d1b9d3f7376100d2ae5b1b063eb9a52d9f216
Laisky <ppcelery@gmail.com>
2014-12-30 22:18:29 +0800

    fix css

--- src/laishime/static/js/topic_tweets.js
+++ src/laishime/static/js/topic_tweets.js
@@ -1,34 +1,36 @@
-$(function(){
+$(function() {
     get_last_update_topics();
     get_most_post_topics();
 });
 
 
-function bind_topics(){
-    $("ul.topics").children().click(function(){
+function bind_topics() {
+    $("ul.topics").children().click(function() {
         var topic = arguments[0].target.innerText;
         get_tweets_by_topic(topic);
     });
 }
 
 
-function get_tweets_by_topic(topic){
+function get_tweets_by_topic(topic) {
     var url = "get-tweets-by-topic";
-    var data = {"topic": topic};
+    var data = {
+        "topic": topic
+    };
 
-    $.getJSON(url, data, function(data){
-        if(data.status != 1) return;
+    $.getJSON(url, data, function(data) {
+        if (data.status != 1) return;
 
         $("#tweets").html(data.data);
     });
 }
 
 
-function get_last_update_topics(){
+function get_last_update_topics() {
     var url = "get-last-update-topics";
 
-    $.getJSON(url, function(data){
-        if(data.status != 1) return;
+    $.getJSON(url, function(data) {
+        if (data.status != 1) return;
 
         $("#last-update-topics").html(data.data);
         bind_topics();
@@ -36,11 +38,11 @@ function get_last_update_topics(){
 }
 
 
-function get_most_post_topics(){
+function get_most_post_topics() {
     var url = "get-most-post-topics";
 
-    $.getJSON(url, function(data){
-        if(data.status != 1) return;
+    $.getJSON(url, function(data) {
+        if (data.status != 1) return;
 
         $("#most-post-topics").html(data.data);
         bind_topics();
